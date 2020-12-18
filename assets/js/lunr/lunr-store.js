9ab5dcb12dedeee1469307a28abcc75f244827a1
Yurii Zubkov <zubkovyuriy@gmail.com>
2020-12-18 15:16:55 +0100

    deploy

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,7 +1,13 @@
 var store = [{
         "title": "RK002 + Novation Circuit = Song Mode",
-        "excerpt":"Article will be here soon, I promise…   Action Sequencer  ","categories": ["Retrokits"],
+        "excerpt":"That was interesting project for the cool guys from Retrokits.com:               All code and description is available on github, feel free to use, fork:   https://github.com/yuriizubkov/rk002-circuit-song-mode https://github.com/yuriizubkov/rk002-sequence-editor   Live Sequence Editor  ","categories": ["Retrokits"],
         "tags": ["Retrokits","RK002","Novation","Circuit"],
         "url": "https://lensflare.dev/retrokits/2020/11/24/rk002-circuit-song-mode.html",
+        "teaser": null
+      },{
+        "title": "Core MIDI Cheat Sheet",
+        "excerpt":"Made myself a cheat sheet of the Core MIDI functions. All functions and structure names are clickable in the PDF version. Maybe it could be useful to someone else.                                                    PNG version       Download PDF version Download LibreOffice file  ","categories": ["Apple"],
+        "tags": ["Apple","iOS","Mac OS","Core MIDI"],
+        "url": "https://lensflare.dev/apple/2020/12/18/core-midi-cheat-sheet.html",
         "teaser": null
       }]
