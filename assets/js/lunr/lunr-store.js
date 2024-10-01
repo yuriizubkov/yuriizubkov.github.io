81dcd06e40c7b57bf917298c0ffa2298ac9184b1
Yurii Zubkov <zubkovyuriy@gmail.com>
2024-10-01 13:44:56 +0200

    deploy

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "RK002 + Novation Circuit = Song Mode",
+        "excerpt":"That was interesting project for the cool guys from Retrokits.com:               All code and description is available on github, feel free to use, fork:   https://github.com/yuriizubkov/rk002-circuit-song-mode https://github.com/yuriizubkov/rk002-sequence-editor   Live Sequence Editor  ","categories": ["Retrokits"],
+        "tags": ["Retrokits","RK002","Novation","Circuit"],
+        "url": "/retrokits/2020/11/24/rk002-circuit-song-mode.html",
+        "teaser": null
+      },{
+        "title": "Core MIDI Cheat Sheet",
+        "excerpt":"Made myself a cheat sheet of the Core MIDI functions. All functions and structure names are clickable in the PDF version. Maybe it could be useful to someone else.                                                    PNG version       Download PDF version Download LibreOffice file  ","categories": ["Apple"],
+        "tags": ["Apple","iOS","Mac OS","Core MIDI"],
+        "url": "/apple/2020/12/18/core-midi-cheat-sheet.html",
+        "teaser": null
+      },{
+        "title": "RK006 Manager",
+        "excerpt":"Made an alternative companion application “RK006 Manager” for the RK006 MIDI/Gate hub from Retrokits. Supports dark and light system UI theme. Available on iPad, iPhone and Mac. And it is free.                                                                                                                                                                                                                                                  Screenshots       Download from Apple App Store  ","categories": ["Retrokits"],
+        "tags": ["Apple","iOS","Mac OS","Retrokits","RK006"],
+        "url": "/retrokits/2021/03/21/rk006-manager-app.html",
+        "teaser": null
+      },{
+        "title": "Online Circuit Patch Converter",
+        "excerpt":"Made a small web-based tool for fast conversion between Novation Circuit patches and Circuit Tracks patches (and vise versa). Patch files are actually the same, except SysEx MIDI Product ID byte, which I am swapping with this converter: Patch Packs from the original Circuit will be unpacked as a separate...","categories": ["Novation"],
+        "tags": ["Novation","Circuit","Circuit Tracks"],
+        "url": "/novation/2021/04/27/circuit-patch-converter.html",
+        "teaser": null
+      }]
